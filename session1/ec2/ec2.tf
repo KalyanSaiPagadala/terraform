3d4ec757b20cafecbb5a9f576a7e9fc4ec4f0541
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-01-03 21:33:00 +0530

    terraform-practice

---
+++ session1/ec2/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb" # devops-practice
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Helloterraform" # instance name
+  }
+}
+
