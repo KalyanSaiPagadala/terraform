15ef4bccaaa944cb62d890aff6637aebdfbb31c7
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-01-05 19:55:34 +0530

    terraform-practice

--- variable/variable.tf
+++ variable/variable.tf
@@ -18,3 +18,25 @@ variable "tags" {
     terraform = "true"
   }
 }
+
+variable "sg-name" {
+  type = string
+  default = "demo-sg"
+}
+
+variable "sg-description" {
+  type = string
+  default = "allow all tcp ports"
+}
+
+variable "inbound-port" {
+  type = number
+  default = 0
+}
+
+variable "cidr-block" {
+  type = list
+  default = ["0.0.0.0/0"]
+}
+
+
