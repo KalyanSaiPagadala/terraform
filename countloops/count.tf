0dd81c965ef8a1b3f9ef4324dab7a22d8a50a085
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-01-07 12:08:02 +0530

    terraform-practice

---
+++ countloops/count.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+    ami           =  var.ami-id# devops-practice
+    count = 3
+    instance_type = "t2.micro"
+    #vpc_security_group_ids = [aws_security_group.demo-sg.id] # this is list
+
+    tags = {
+        name = "web"
+    }
+}
