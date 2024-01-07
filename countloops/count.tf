c0b2aac294251c62f55537d8414750017a8032a3
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-01-07 13:36:02 +0530

    terraform-practice

--- countloops/count.tf
+++ countloops/count.tf
@@ -1,10 +1,17 @@
 resource "aws_instance" "web" {
+    count = 4 
     ami           =  var.ami-id# devops-practice
-    count = 3
-    instance_type = "t2.micro"
-    #vpc_security_group_ids = [aws_security_group.demo-sg.id] # this is list
-
+    instance_type = var.instances-name[count.index] == "mongodb" || var.instances-name[count.index] == "mysql" ? "t3.micro" : "t2.micro"
     tags = {
-        name = var.instances-name[count.index]
+        Name = var.instances-name[count.index]
     }
 }
+
+resource "aws_route53_record" "www" {
+    count = 4
+    zone_id = var.zone-id
+    name    = "${var.instances-name[count.index]}.${var.sd-name}"  # dns record name ${} are used for interpolation
+    type    = "A"
+    ttl     =1
+    records = var.instances-name[count.index] == "web" ? [aws_instance.web[count.index].public_ip] : [aws_instance.web[count.index].private_ip]         # which type of ip shoud be attached to the specific record
+    }
--- countloops/variables.tf
+++ countloops/variables.tf
@@ -5,5 +5,14 @@ variable "ami-id" {
 
 variable "instances-name" {
   type = list
-  default = ["mongodb", "catalogue" , "web"]
+  default = ["mongodb", "catalogue" , "web","mysql"]
+  }
+
+variable "sd-name" {
+  type = string
+  default = "dawshub.online"
+  }
+
+variable "zone-id" {
+  default = "Z04463933CB28G174P97V"
   }
