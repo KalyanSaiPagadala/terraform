1c219ab96f4d9619687296ab55c26a6d26c45dc2
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-01-17 14:20:48 +0530

    terraform-practice

---
+++ remotestate/variables.tf
@@ -0,0 +1,22 @@
+variable "ami-id" {
+  type = string
+  default = "ami-081609eef2e3cc958"
+}
+
+variable "instances-name" {
+  type = map
+  default = {
+    mongodb = "t3.micro"
+    catalogue = "t2.micro"
+    web = "t2.micro"
+  }
+  }
+
+variable "sd-name" {
+  type = string
+  default = "dawshub.online"
+  }
+
+variable "zone-id" {
+  default = "Z04463933CB28G174P97V"
+  }
